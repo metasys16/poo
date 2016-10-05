8f9d6d8f3320dfe5e841c0391ecb42e37e0f54e7
metasys16 <pereira.mickael1@gmail.com>
2016-10-05 13:46:08 +0200

    Définition de la classe contact

---
+++ Objet/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objet
+{
+    class Contact
+    {
+        private String nom;
+        private String prenom;
+        private String telephone;
+        private String mail;
+        private DateTime dateDeNaissance;
+    }
+}
